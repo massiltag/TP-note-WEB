c17948360a3e98fa793de94c3fcbc330447960d9
Nelly Ung <56115087+Nellyuki@users.noreply.github.com>
2021-04-07 11:48:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function makeAjaxCall(url, methodType, callback) {
+
+    let header = document.querySelector('header');
+    let section = document.querySelector('section');
+
+    return $.ajax({
+        url: url,
+        method: methodType,
+        dataType: "json"
+    })
+}
+
+
 function setEvents() {
     $("#search").click(function(){
         req();
@@ -7,4 +20,21 @@ function setEvents() {
 
 function req() {
     alert('requête');
+}
+
+function req2() {
+    //coordonnees de Paris 
+    let champ1 = document.getElementById("champ1").value;
+    let champ2 = document.getElementById("champ2").value;
+    let champ3 = document.getElementById("champ3").value;
+    let URL = "";
+
+    makeAjaxCall(URL, "GET").then(function (respJson) {
+       
+        
+
+    }, function (reason) {
+        console.log("error in processing your request", reason);
+    });
+
 }

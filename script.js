d36a90f80df900235be3803e5ce453857f480230
Nelly Ung <56115087+Nellyuki@users.noreply.github.com>
2021-04-07 15:28:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function makeAjaxCall(url, methodType, callback) {
 
 function setEvents() {
     $("#search").click(function(){
-        req();
+        req2();
     });
 }
 
@@ -22,16 +22,28 @@ function req() {
     alert('requête');
 }
 
+//zip code api
 function req2() {
     //coordonnees de Paris 
-    let champ1 = document.getElementById("champ1").value;
-    let champ2 = document.getElementById("champ2").value;
-    let champ3 = document.getElementById("champ3").value;
-    let URL = "";
+    let ville = document.getElementById("champ1").value;
+    let codePays = document.getElementById("champ2").value;
+    let langue = document.getElementById("champ3").value;
+    let units = document.getElementById("champ4").value;
+   
+    let apiKey="ecaa16f9b4b360ea8b57867cd78a6816"
+
+    let URL = "http://api.openweathermap.org/data/2.5/weather?q="+ville+","+codePays+"&lang="+langue+"&units="+units+'&appid='+apiKey;
 
     makeAjaxCall(URL, "GET").then(function (respJson) {
+
+        //test =respJson.dataseries[0].timepoint
+        console.log(respJson.weather[0].description);
+        console.log("temperature moyenene" + respJson.main.temp);
+        console.log("temperature ressentie" + respJson.main.feels_like);
+        console.log("temperature min" + respJson.main.temp_min);
+        console.log("temperature max" + respJson.main.temp_max);
+       // $('#table tr:last').append('<td>' + test + '</td>');
        
-        
 
     }, function (reason) {
         console.log("error in processing your request", reason);
